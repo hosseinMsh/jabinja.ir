7a1e691c77367c6ee24d59d3abeb571c60da5b6d
mac <mac@apple.com>
2026-06-02 23:39:57 +0330

    feat: افزودن ثابت‌های اپلیکیشن شامل نام و آدرس API

---
+++ lib/utils/constants.dart
@@ -0,0 +1,7 @@
+class Constants {
+  static const String appName = 'جابینجا';
+  static const String baseUrl = 'http://localhost:3000/api';
+
+  static const String tokenKey = 'auth_token';
+  static const String userKey = 'user_data';
+}
