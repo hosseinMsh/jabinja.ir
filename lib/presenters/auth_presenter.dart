6a1530a6dfcb36ce84a0bdc060ca61b277e7fc9b
mohammadmehdi42189 <seyyed.emami844545@gmail.com>
2026-06-06 23:22:13 +0330

    Use local signup accounts as login fallback

--- lib/presenters/auth_presenter.dart
+++ lib/presenters/auth_presenter.dart
@@ -1,6 +1,7 @@
 import '../models/user.dart';
 import '../services/api_service.dart';
 import '../services/real_api_service.dart';
+import '../services/session_manager.dart';
 
 abstract class AuthView {
   void onLoginSuccess(User user);
@@ -15,29 +16,47 @@ abstract class AuthView {
 class AuthPresenter {
   final AuthView _view;
   final ApiService _apiService = RealApiService();
+  final SessionManager _sessionManager = SessionManager();
 
   AuthPresenter(this._view);
 
   void login(String email, String password) {
     _view.setLoading(true);
-    _apiService.login(email, password).then((response) {
-      _view.setLoading(false);
+    _apiService.login(email, password).then((response) async {
       if (response.success && response.data != null) {
+        await _sessionManager.saveSession(response.data!);
+        _view.setLoading(false);
         _view.onLoginSuccess(response.data!);
-      } else {
-        _view.onLoginError(response.message ?? 'خطا در ورود');
+        return;
       }
-    }).catchError((_) {
+
+      final localUser = await _sessionManager.findLocalAccount(email, password);
       _view.setLoading(false);
-      _view.onLoginError('خطا در ارتباط با سرور');
+      if (localUser != null) {
+        await _sessionManager.saveSession(localUser);
+        _view.onLoginSuccess(localUser);
+      } else {
+        _view.onLoginError(response.message ?? 'ایمیل یا رمز عبور اشتباه است');
+      }
+    }).catchError((_) async {
+      final localUser = await _sessionManager.findLocalAccount(email, password);
+      _view.setLoading(false);
+      if (localUser != null) {
+        await _sessionManager.saveSession(localUser);
+        _view.onLoginSuccess(localUser);
+      } else {
+        _view.onLoginError('خطا در ارتباط با سرور');
+      }
     });
   }
 
   void signup(String name, String email, String password) {
     _view.setLoading(true);
-    _apiService.signup(name, email, password).then((response) {
+    _apiService.signup(name, email, password).then((response) async {
       _view.setLoading(false);
       if (response.success && response.data != null) {
+        await _sessionManager.saveSession(response.data!);
+        await _sessionManager.saveLocalAccount(response.data!, password);
         _view.onSignupSuccess(response.data!);
       } else {
         _view.onSignupError(response.message ?? 'خطا در ثبت‌نام');
